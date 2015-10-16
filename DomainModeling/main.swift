71526017dde71a79a8246d52f58a7d50ab8efad6
xyju <xyju90@gmail.com>
2015-10-15 17:15:28 -0700

    homework

--- DomainModeling/main.swift
+++ DomainModeling/main.swift
@@ -28,69 +28,69 @@ struct Money {
     var currency: Currency
     
     //Convert
-    func convert(money: Money, currency: Currency) -> Money {
+    func convert(convertCurrency: Currency) -> Money {
         
-        if(money.currency == currency) {
+        if(currency == convertCurrency) {
             
-            return money
+            return self
             
         } else {
             
-            switch(money.currency, currency) {
+            switch(currency, convertCurrency) {
                 
             case(.USD, .GBP):
                 
-                return Money(amount: money.amount * 0.5, currency: currency)
+                return Money(amount: amount * 0.5, currency: convertCurrency)
                 
             case(.USD, .EUR):
                 
-                return Money(amount: money.amount * 1.5, currency: currency)
+                return Money(amount: amount * 1.5, currency: convertCurrency)
                 
             case(.USD, .CAN):
                 
-                return Money(amount: money.amount * 1.25, currency: currency)
+                return Money(amount: amount * 1.25, currency: convertCurrency)
                 
             case(.GBP, .USD):
                 
-                return Money(amount: money.amount * 2, currency: currency)
+                return Money(amount: amount * 2, currency: convertCurrency)
                 
             case(.GBP, .EUR):
                 
-                return Money(amount: money.amount * 3, currency: currency)
+                return Money(amount: amount * 3, currency: convertCurrency)
                 
             case(.GBP, .CAN):
                 
-                return Money(amount: money.amount * 2.5, currency: currency)
+                return Money(amount: amount * 2.5, currency: convertCurrency)
                 
             case(.EUR, .USD):
                 
-                return Money(amount: money.amount * 2 / 3, currency: currency)
+                return Money(amount: amount * 2 / 3, currency: convertCurrency)
                 
             case(.EUR, .GBP):
                 
-                return Money(amount: money.amount / 3, currency: currency)
+                return Money(amount: amount / 3, currency: convertCurrency)
                 
             case(.EUR, .CAN):
                 
-                return Money(amount: money.amount * 5 / 6, currency: currency)
+                return Money(amount: amount * 5 / 6, currency: convertCurrency)
                 
             case(.CAN, .USD):
                 
-                return Money(amount: money.amount * 0.8, currency: currency)
+                return Money(amount: amount * 0.8, currency: convertCurrency)
                 
             case(.CAN, .GBP):
                 
-                return Money(amount: money.amount * 0.4, currency: currency)
+                return Money(amount: amount * 0.4, currency: convertCurrency)
                 
             case(.CAN, .EUR):
                 
-                return Money(amount: money.amount * 1.2, currency: currency)
+                return Money(amount: amount * 1.2, currency: convertCurrency)
                 
             default:
                 
                 print("Input Error")
                 
-                return money
+                return self
                 
             }
             
@@ -98,66 +98,54 @@ struct Money {
         
     }
     
+    
     // Array Add
     func add(moneys: [Money]) -> Money {
         
-        var amount = 0.0
-        
-        var money = Money(amount: 0.0, currency: .USD)
+        var amount = self.amount
         
         for index in 0...moneys.count - 1{
             
-            money = convert(moneys[index], currency: .USD)
-            
-            amount = amount + money.amount
+            amount = amount + moneys[index].convert(self.currency).amount
             
         }
         
-        return Money(amount: amount, currency: .USD)
+        return Money(amount: amount, currency: self.currency)
         
     }
     
     //Array Subtract
     func sub(moneys: [Money]) -> Money {
         
-        var totleAmount = moneys[0].amount
-        
-        var amount = 0.0
-        
-        var money = Money(amount: 0.0, currency: .USD)
+        var amount = self.amount
         
         for index in 0...moneys.count - 1{
             
-            money = convert(moneys[index], currency: .USD)
-            
-            if(index == 0){
-                
-                amount = 0
-                
-            } else {
-                
-                amount = money.amount
-            }
-            
-            totleAmount = totleAmount - amount
+            amount = amount - moneys[index].convert(self.currency).amount
             
         }
         
-        return Money(amount: totleAmount, currency: .USD)
+        return Money(amount: amount, currency: self.currency)
         
     }
     
 }
 
 // Unit Test: Money Convert
-var currentMoney = Money(amount: 60, currency: .EUR)
+var currentMoney1 = Money(amount: 60, currency: .EUR)
 
-var convertMoney = currentMoney.convert(currentMoney, currency: .USD)
+var convertMoney1 = currentMoney1.convert(.USD)
 
-print(convertMoney.amount, convertMoney.currency)
+print(convertMoney1.amount, convertMoney1.currency)
+
+var currentMoney2 = Money(amount: 60, currency: .CAN)
+
+var convertMoney2 = currentMoney2.convert(.USD)
+
+print(convertMoney2.amount, convertMoney2.currency)
 
 // Unit Test: Money Add
-var money = Money(amount:0, currency:.USD)
+var money = Money(amount:3, currency:.EUR)
 
 var totleMoney = money.add([Money(amount: 3, currency: .USD), Money(amount: 9, currency: .EUR), Money(amount: 5, currency: .CAN)])
 
@@ -170,13 +158,29 @@ print(totleMoney.amount, totleMoney.currency)
 
 
 // Job
+enum Per{
+    
+    case hour
+    
+    case year
+}
+
+
+struct Salary{
+    
+    var money: Money
+    
+    var per: Per
+}
+
+
 class Job {
     
     var title: String
     
-    var salary: Money
+    var salary: Salary
     
-    init(currentTitle: String, currentSalary: Money) {
+    init(currentTitle: String, currentSalary: Salary) {
         
         title = currentTitle
         
@@ -184,31 +188,50 @@ class Job {
         
     }
     
-    func calculateIncome(hour: Double) -> Money {
+    func calculateIncome(hours: Double) -> Money {
         
-        return Money(amount: salary.amount * hour, currency: salary.currency)
+        if salary.per == .hour {
+            
+            return Money(amount: salary.money.amount * hours, currency: salary.money.currency)
+            
+        }
+        
+        return salary.money
+        
     }
     
-    func raise(percentage: Double) -> Money {
+    func raise(percentage: Double) -> Salary {
         
-        return Money(amount: salary.amount * (1 + percentage), currency: salary.currency)
+        return Salary(money: Money(amount: salary.money.amount * (1 + percentage),currency: salary.money.currency), per: salary.per)
 
     }
 }
 
 //Unit Test: Job CalculateIncome
-var job1 = Job(currentTitle: "engineer", currentSalary: Money(amount:20, currency: .USD))
+var job1 = Job(currentTitle: "engineer", currentSalary: Salary(money: Money(amount: 20, currency: .USD), per: .hour))
 
-var moneyPerHour = job1.calculateIncome(1500)
+var moneyPerYear1 = job1.calculateIncome(1500)
 
-print(moneyPerHour.amount, moneyPerHour.currency)
+print(moneyPerYear1.amount, moneyPerYear1.currency)
+
+var job2 = Job(currentTitle: "engineer", currentSalary: Salary(money: Money(amount: 20000, currency: .CAN), per: .year))
+
+var moneyPerYear2 = job2.calculateIncome(1500)
+
+print(moneyPerYear2.amount, moneyPerYear2.currency)
 
 //Unit Test: Job Raise
-var job2 = Job(currentTitle: "engineer", currentSalary: Money(amount:50000, currency: .USD))
+var salary1 = job1.raise(0.2)
 
-var nextYearMoney = job2.raise(0.20)
+print(salary1.money.amount, salary1.money.currency, salary1.per)
 
-print(nextYearMoney.amount, nextYearMoney.currency)
+var salary2 = job2.raise(0.2)
+
+print(salary2.money.amount, salary2.money.currency, salary2.per)
+
+//var nextYearMoney = job2.raise(0.20)
+
+//print(nextYearMoney.amount, nextYearMoney.currency)
 
 
 class Person {
@@ -234,23 +257,118 @@ class Person {
     //        job ＝; currentJob!
     //    }
     
-    init(first: String, last: String, currentAge: Int) {
+    init(first: String, last: String, age: Int, job: Job?, spouse: Person?) {
         
         firstName = first
         
         lastName = last
         
-        age = currentAge
+        self.age = age
+        
+        self.job = job
+        
+        self.spouse = spouse
+        
+        if self.age < 16{
+            
+            if self.job != nil{
+                
+                self.job = nil
+                
+                print("The person cannot have a job!")
+                
+            }
+            
+        }
+        
+        if self.age < 18{
+            
+            if self.spouse != nil{
+                
+                self.spouse = nil
+                
+                print("The person cannot have a spouse!")
+                
+            }
+        }
     }
     
-    func toString() -> String {
-        return job!.title
+    func toString() {
+        
+        print("FirstName:" , self.firstName , "LastName:" , self.lastName , "Age:" ,self.age, "Job Title:" , self.job!.title, "Job Salary:", self.job!.salary.money.amount, self.job!.salary.money.currency, "Per:", self.job!.salary.per)
+        
+        if self.spouse != nil{
+            
+            print("spouse:")
+            
+            self.spouse!.toString()
+        }
     }
-    
-    
     
 }
+
+//Test
+var person1 = Person(first: "Xiangyu", last: "Ju", age: 25, job: job1, spouse: nil)
+
+person1.toString()
+
+var person3 = Person(first: "Lily", last: "Joe", age: 23, job: job1, spouse: nil)
+
+var person2 = Person(first: "Zixuan", last: "Wang", age: 23, job: job2, spouse: person3)
+
+person2.toString()
+
+
 //Family
+class Family{
+    
+    var members:[Person]
+    
+    init(members: [Person]) {
+        
+        self.members = members
+        
+        var isFamily = false
+        
+        for index in 0...self.members.count - 1 {
+            
+            if self.members[index].age > 21{
+                
+                isFamily = true
+                
+            }
+        }
+        
+        if !isFamily{
+            
+            print("The family is not legal!")
+            
+        }
+    }
+    
+    func householdIncome() -> Money{
+        
+        var moneys = [Money]()
+        
+        moneys[0] = (members[0].job?.salary.money)!
+        
+        for index in 1...members.count - 1{
+            
+            moneys.append(members[index].job!.salary.money)
+        }
+        
+        return (members[0].job?.salary.money.add(moneys))!
+        
+    }
+    
+    func haveChild() -> Family{
+        
+        self.members.append(Person(first:"N/A", last: "N/A", age:0, job: nil, spouse: nil))
+        
+        return self
+        
+    }
+}
 
 
 
